e7cf5f85b6c3e05d48d8dab6a9711799810fd2c5
Vũ Đăng Huy <moonjaeshin@Vus-iMac-Pro.local>
2021-03-01 01:05:41 +0700

    initial project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Layout } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+
+import Home from "./views/Home";
+import Marketplace from "./views/Marketplace";
+import HorizontalMenu from "./components/Navigation/HorizontalMenu";
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const App = () => (
+  <Layout class="container">
+    <Header>
+      <HorizontalMenu />
+    </Header>
+    <Layout>
+      <Sider>Sider</Sider>
+      <Content>Content</Content>
+    </Layout>
+    <Footer>Footer</Footer>
+
+    <Router>
+      <Switch>
+        <Route path="/marketplace" exact>
+          <Marketplace />
+        </Route>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  </Layout>
+);
 
 export default App;
